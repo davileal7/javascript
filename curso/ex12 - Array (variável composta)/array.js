cfad59791ea8ab5a104a7e2e12d766388305a46c
Davi Leal <davi.m.leal@gmail.com>
2022-10-09 21:43:28 -0300

    atualizado teste

---
+++ curso/ex12 - Array (variável composta)/array.js
@@ -0,0 +1,13 @@
+var jogos = ['Resident Evil 1']
+
+console.log(jogos)
+
+if (jogos == 'Resident Evil 1') {
+    console.log('the best')
+
+} else if (jogos = 'Dino Crises') {
+    console.log('Bom também!!!')
+
+} else if (jogos == 'Resident Evil 5') { 
+    console.log('jogo ruim')
+}
--- curso/ex12 - Array (variável composta)/script.js
+++ curso/ex12 - Array (variável composta)/script.js
@@ -1,12 +1,16 @@
-var num = [5,8,2,9,3]     //array []
+var num = [5,8,2,9,3]     //array/vetor []
 console.log(num)
 console.log(`O vetor tem ${num.length} posições.`)
 console.log(`o primeiro valor do vetor é ${num[0]}`)
 var pos = num.indexOf(8)
-console.log(`O valor 8 está na ${pos}`)
+console.log(`O valor 8 está na ${pos}° posição`)
 
 
 num[3] = 9 //acrescentar em 3°
 num.push(7) //acrescentar em último
 num.sort() //arrumar em crescente
 console.log(num)
+
+
+var nomes = ['davi','julia','edson']
+console.log(nomes[2]) 
